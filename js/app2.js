2fdea2fcad42a2f8cb8639b990c21612a604bc44
mel1bt <mberrocal.tipte@gmail.com>
2016-08-25 15:43:46 -0500

    segundo ejercicio js

---
+++ js/app2.js
@@ -0,0 +1,7 @@
+var num1 = parseInt(prompt("Ingrese 1er numero"));
+var num2 = parseInt( prompt("Ingrese 2do numero"));
+var num3 = parseInt( prompt("Ingrese  3er numero"));
+var suma=0, prom=0;
+suma = num1 + num2 +  num3; 
+prom = suma / 3;
+document.write("El promedio es " + prom );
